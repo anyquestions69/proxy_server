2693d83eadf149de0846cd49dc89fd85ccbf7f10
public_hysteria <juuzodes@yandex.ru>
2024-04-10 22:40:01 +0300

    static

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ let proxy = require('express-http-proxy');
 const express = require('express')
 const app = express()
 
-app.use('/static', proxy('https://tinder.com/static/build',{
-    userResDecorator: (proxyRes, proxyResData) => {
-        let _data = JSON.parse(proxyResData);
-        _data.proxy = true;
-        return JSON.stringify(_data);
-    },
+app.use('/static', proxy('https://tinder.com/static',{
+    https:true
 }));
 
 app.listen('3000')
