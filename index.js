cd5900c6c49073a8bf2e58f534a90cfaf5e47b86
anyquestions69 <danil_tyrenko@icloud.com>
2024-04-17 12:02:56 +0300

    upd

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let proxy = require('express-http-proxy');
 const express = require('express')
 const app = express()
+app.use('/api', proxy('https://api.gotinder.com'))
 
-app.use('/static', proxy('https://tinder.com/static',{
+app.use('/', proxy('https://tinder.com',{
     https:true
 }));
 
+//app.get('/', (req,res)=>{return res.redirect('/static')})
 app.listen('3000')
